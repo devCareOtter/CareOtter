38b501998d5f99962d79269a8ae109b4ffb02197
rdurham <ryan.durham@careotter.com>
2015-05-07 14:28:02 -0500

    CS client sample

---
+++ CS/SampleProject/SampleProject/BondConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProject
+{
+    /// <summary>
+    /// Handles conversions specified in bond schema file
+    /// </summary>
+    public static class BondTypeAliasConverter
+    {
+        public static long Convert(DateTime value, long unused)
+        {
+            return value.Ticks;
+        }
+
+        public static DateTime Convert(long value, DateTime unused)
+        {
+            return new DateTime(value);
+        }
+
+        public static Guid Convert(string value, Guid unused)
+        {
+            return new Guid(value);
+        }
+
+        public static string Convert(Guid value, string unused)
+        {
+            if (value == Guid.Empty)
+                throw new ArgumentException("Empty guid has issues");
+
+            return value.ToString();
+        }
+    }
+}
